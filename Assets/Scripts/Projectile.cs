6c2fff70a376d36b5cc0faabdcbd93b852e2ed50
kirinzloh <weiquan7@hotmail.com>
2017-04-01 16:38:16 +0800

    Tower projectile logic added, without integration with towers and monsters

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    private Monster target;
+    private Tower towerType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AttackTarget();
+	}
+
+    public void Initialize(Tower towerType)
+    {
+        this.target = towerType.GetTarget;
+        this.towerType = towerType;
+    }
+    private void AttackTarget()
+    {
+        if (target != null && target.isActive)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * towerType.projectileSpeed);
+        }
+        else if (!target.isActive)
+        {
+            GameplayManager.Instance.Pool.ReleaseObject(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Monster")
+        {
+            if (target.gameObject == other.gameObject)
+            {
+                target.TakeDamage(towerType.TowerDamage);
+            }
+            GameplayManager.Instance.Pool.ReleaseObject(gameObject);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Unity2DGame-TowerOffense
-A 2-player tower defense game using Unity 5.5.
+A 2-player tower defense game using Unity 5.5.  
+Each player will have their own map and they will build towers to defend against the incoming monsters sent by the opposing player.   
+The objective of the game is to reduce the opposing player’s HP to 0!   
+
+
