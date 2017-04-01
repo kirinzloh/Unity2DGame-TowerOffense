3687cc67d0a5742c7223e33f1699b8cb9f724c4a
kirinzloh <weiquan7@hotmail.com>
2017-04-01 17:07:50 +0800

    BuildRoute Interface Adjustment

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -3,18 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour {
-
-<<<<<<< HEAD
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-=======
     public enum State { EMPTY, TOWER, PATH, OBSTACLE}
 
     public Coord coord;
@@ -36,5 +24,4 @@ public class Tile : MonoBehaviour {
     // Update is called once per frame
     void Update () {
     }
->>>>>>> aadbf5dc797015b119d77c0094c605ccaf64d02a
 }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,105 +1,105 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Tower : MonoBehaviour {
-    // This is the projectile's type
-    public string projectileType;
+//public class Tower : MonoBehaviour {
+//    // This is the projectile's type
+//    public string projectileType;
 
-    // This is the target of the tower's attack
-    private Monster target;
+//    // This is the target of the tower's attack
+//    private Monster target;
 
-    // This is the tower's target queue
-    private Queue<Monster> monsters = new Queue<Monster>();
+//    // This is the tower's target queue
+//    private Queue<Monster> monsters = new Queue<Monster>();
 
-    // This indicates whether the tower can attack (shoot projectile)
-    private bool canAttack = true;
+//    // This indicates whether the tower can attack (shoot projectile)
+//    private bool canAttack = true;
 
-    // This is the timer for the tower's attack
-    private float attackTimer;
+//    // This is the timer for the tower's attack
+//    private float attackTimer;
 
-    // This is the tower's attack speed (Number of secs per attack)
-    public float attackSpeed;
+//    // This is the tower's attack speed (Number of secs per attack)
+//    public float attackSpeed;
 
-    // This is the tower's projectile speed
-    public float projectileSpeed;
+//    // This is the tower's projectile speed
+//    public float projectileSpeed;
 
-    // This is the tower's damage
-    public int towerDamage;
+//    // This is the tower's damage
+//    public int towerDamage;
 
-    // Use this for initialization
-    void Start () {
+//    // Use this for initialization
+//    void Start () {
         
-	}
+//	}
 	
-	// Update is called once per frame
-	void Update () {
-        Attack();
-	}
+//	// Update is called once per frame
+//	void Update () {
+//        Attack();
+//	}
 
-    private void Attack()
-    {
-        if (!canAttack)
-        {
-            attackTimer += Time.deltaTime;
-            if (attackTimer >= attackSpeed)
-            {
-                canAttack = true;
-                attackTimer = 0;
-            }
-        }
-        if (target == null && monsters.Count > 0)
-        {
-            target = monsters.Dequeue();
-        }
-        if (target != null && target.isActive)
-        {
-            if (canAttack == true)
-            {
-                Shoot();
-                canAttack = false;
-            }
-        }
-    }
+//    private void Attack()
+//    {
+//        if (!canAttack)
+//        {
+//            attackTimer += Time.deltaTime;
+//            if (attackTimer >= attackSpeed)
+//            {
+//                canAttack = true;
+//                attackTimer = 0;
+//            }
+//        }
+//        if (target == null && monsters.Count > 0)
+//        {
+//            target = monsters.Dequeue();
+//        }
+//        if (target != null && target.isActive)
+//        {
+//            if (canAttack == true)
+//            {
+//                Shoot();
+//                canAttack = false;
+//            }
+//        }
+//    }
 
-    private void Shoot()
-    {
-        Projectile projectile = GameplayManager.Instance.Pool.GetObject(projectileType).GetComponent<Projectile>();
-        projectile.transform.position = transform.position;
-        projectile.Initialize(this);
-    }
+//    private void Shoot()
+//    {
+//        Projectile projectile = GameplayManager.Instance.Pool.GetObject(projectileType).GetComponent<Projectile>();
+//        projectile.transform.position = transform.position;
+//        projectile.Initialize(this);
+//    }
 
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Monster")
-        {
-            monsters.Enqueue(other.GetComponent<Monster>());
-        }
-    }
+//    public void OnTriggerEnter2D(Collider2D other)
+//    {
+//        if (other.tag == "Monster")
+//        {
+//            monsters.Enqueue(other.GetComponent<Monster>());
+//        }
+//    }
 
-    public void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Monster") 
-        {
-            target = null;
-        }
-    }
+//    public void OnTriggerExit2D(Collider2D other)
+//    {
+//        if (other.tag == "Monster") 
+//        {
+//            target = null;
+//        }
+//    }
 
-    public float GetProjectileSpeed
-    {
-        get { return projectileSpeed; }
-    }
+//    public float GetProjectileSpeed
+//    {
+//        get { return projectileSpeed; }
+//    }
 
-    public Monster GetTarget
-    {
-        get { return target; }
-    }
+//    public Monster GetTarget
+//    {
+//        get { return target; }
+//    }
 
-    public int TowerDamage
-    {
-        get
-        {
-            return towerDamage;
-        }
-    }
-}
+//    public int TowerDamage
+//    {
+//        get
+//        {
+//            return towerDamage;
+//        }
+//    }
+//}
