a92d4d935d640decd2a67c9539701c83cdb7cfd0
kirinzloh <weiquan7@hotmail.com>
2017-04-10 13:14:08 +0800

    Added circle tower sprites (alternatives)

--- Assets/Scripts/Data Structures/Coord.cs
+++ Assets/Scripts/Data Structures/Coord.cs
@@ -12,20 +12,4 @@ public struct Coord {
 
     public int row { get { return _row; } }
     public int col { get { return _col; } }
-
-    #region serialisation
-    public int serializeTo(byte[] dest, ref int index) {
-        ExitGames.Client.Photon.Protocol.Serialize(row, dest, ref index);
-        ExitGames.Client.Photon.Protocol.Serialize(col, dest, ref index);
-        return 8;
-    }
-
-    public static Coord deserialize(byte[] from, ref int index) {
-        int d_row;
-        int d_col;
-        ExitGames.Client.Photon.Protocol.Deserialize(out d_row, from, ref index);
-        ExitGames.Client.Photon.Protocol.Deserialize(out d_col, from, ref index);
-        return new Coord(d_row, d_col);
-    }
-    #endregion
 }
