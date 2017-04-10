b45d2f0395036553eac9687ed57bc6a050154d38
kirinzloh <weiquan7@hotmail.com>
2017-04-10 14:40:47 +0800

    Circle tower sprites fixed

--- Assets/Scripts/Data Structures/Coord.cs
+++ Assets/Scripts/Data Structures/Coord.cs
@@ -1,15 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-public struct Coord {
+public struct Coord
+{
     private readonly int _row;
     private readonly int _col;
 
-    public Coord(int row, int col) {
+    public Coord(int row, int col)
+    {
         this._row = row;
         this._col = col;
     }
 
     public int row { get { return _row; } }
     public int col { get { return _col; } }
+
+    #region serialisation
+    public int serializeTo(byte[] dest, ref int index)
+    {
+        ExitGames.Client.Photon.Protocol.Serialize(row, dest, ref index);
+        ExitGames.Client.Photon.Protocol.Serialize(col, dest, ref index);
+        return 8;
+    }
+
+    public static Coord deserialize(byte[] from, ref int index)
+    {
+        int d_row;
+        int d_col;
+        ExitGames.Client.Photon.Protocol.Deserialize(out d_row, from, ref index);
+        ExitGames.Client.Photon.Protocol.Deserialize(out d_col, from, ref index);
+        return new Coord(d_row, d_col);
+    }
+    #endregion
 }
--- Assets/Scripts/Data Structures/MapData.cs
+++ Assets/Scripts/Data Structures/MapData.cs
@@ -1,54 +1,128 @@
 ﻿using System.Collections.Generic;
+using ExitGames.Client.Photon;
 
 
-
-public class MapData {
+public class MapData
+{
 
     public int numRows;
     public int numCols;
 
-    private List<TileData> path;
     private TileData[,] grid;
+    private List<TileData> path;
 
-    public MapData(int i, int j) {
-        numRows = i;
-        numCols = j;
+    public MapData(int r, int c)
+    {
+        numRows = r;
+        numCols = c;
         grid = new TileData[numRows, numCols];
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < numCols; j++)
+            {
+                grid[i, j] = new TileData();
+            }
+        }
         path = new List<TileData>();
     }
 
-    public TileData getTileData(int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.GetLength(0) || j >= grid.GetLength(1)) {
+    public TileData getTileData(int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= grid.GetLength(0) || j >= grid.GetLength(1))
+        {
             return null;
         }
         return grid[i, j];
     }
 
-    public List<TileData> getPath() {
+    public List<TileData> getPath()
+    {
         return path;
     }
 
-    public void setGrid(BuildTile[,] buildGrid) {
-        if (buildGrid.GetLength(0) != numRows || buildGrid.GetLength(1) != numCols) {
+    public void setGrid(BuildTile[,] buildGrid)
+    {
+        if (buildGrid.GetLength(0) != numRows || buildGrid.GetLength(1) != numCols)
+        {
             throw new System.Exception("Grid size mismatch.");
         }
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < numCols; j++)
+            {
                 grid[i, j] = buildGrid[i, j].tileData;
             }
         }
     }
 
-    public void setPath(List<BuildTile> buildPath) {
-        foreach (BuildTile tile in buildPath) {
+    public void setPath(List<BuildTile> buildPath)
+    {
+        foreach (BuildTile tile in buildPath)
+        {
             path.Add(tile.tileData);
         }
     }
+
+    #region serialisation
+    public byte[] serializeNew()
+    {
+        int size = 8; // 2*4 bytes for numRows and numCols.
+        size += numRows * numCols * TileData.serialize_size; // For grid
+        size += 4 + (8 * path.Count); // 2*4 bytes (coordinates) * path length. + 4 bytes path length (int). For path.
+        byte[] mapBytes = new byte[size];
+        int index = 0;
+
+        Protocol.Serialize(numRows, mapBytes, ref index);
+        Protocol.Serialize(numCols, mapBytes, ref index);
+        // Serialise grid
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < numCols; j++)
+            {
+                getTileData(i, j).serializeTo(mapBytes, ref index);
+            }
+        }
+
+        Protocol.Serialize(path.Count, mapBytes, ref index);
+        for (int i = 0; i < path.Count; i++)
+        {
+            path[i].coord.serializeTo(mapBytes, ref index);
+        }
+        return mapBytes;
+    }
+
+    public static MapData deserializeNew(byte[] mapBytes)
+    {
+        int index = 0;
+        int numRows;
+        int numCols;
+        Protocol.Deserialize(out numRows, mapBytes, ref index);
+        Protocol.Deserialize(out numCols, mapBytes, ref index);
+        MapData map = new MapData(numRows, numCols);
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < numCols; j++)
+            {
+                map.getTileData(i, j).deserializeFrom(mapBytes, ref index);
+            }
+        }
+        int pathLength;
+        Protocol.Deserialize(out pathLength, mapBytes, ref index);
+        List<TileData> path = map.getPath();
+        for (int i = 0; i < pathLength; i++)
+        {
+            Coord c = Coord.deserialize(mapBytes, ref index);
+            path.Add(map.getTileData(c.row, c.col));
+        }
+        return map;
+    }
+    #endregion
 }
 
 
 
-public class TileData {
+public class TileData
+{
     public enum State { EMPTY, TOWER, PATH, OBSTACLE }
     public enum Direction { UNSET, UP, DOWN, LEFT, RIGHT }
 
@@ -56,4 +130,34 @@ public class TileData {
     public State state; // Default EMPTY
     public Direction startDirection; // Default UNSET
     public Direction endDirection; // Default UNSET
+    public int towerType; // Default 0. 0 is no tower.
+
+    public const int serialize_size = 15; // Coord: 2*4byte (int). State + Direction: converted into 1 byte. 3*1 byte. towerType: 4byte (int).
+
+    #region serialisation
+    public int serializeTo(byte[] dest, ref int index)
+    {
+        coord.serializeTo(dest, ref index);
+        dest[index] = (byte)state;
+        ++index;
+        dest[index] = (byte)startDirection;
+        ++index;
+        dest[index] = (byte)endDirection;
+        ++index;
+        Protocol.Serialize(towerType, dest, ref index);
+        return serialize_size;
+    }
+
+    public void deserializeFrom(byte[] from, ref int index)
+    {
+        coord = Coord.deserialize(from, ref index);
+        state = (TileData.State)from[index];
+        ++index;
+        startDirection = (TileData.Direction)from[index];
+        ++index;
+        endDirection = (TileData.Direction)from[index];
+        ++index;
+        Protocol.Deserialize(out towerType, from, ref index);
+    }
+    #endregion
 }
