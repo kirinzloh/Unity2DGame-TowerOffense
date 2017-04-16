7f9c1c2176b733b6c5e2e5ab3dec297cca0b8952
coffeebenzene <ericteo_no1@hotmail.com>
2017-04-16 18:22:01 +0800

    folder rework, +back to exit

---
+++ Assets/Scripts/UI scripts/BackHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackHandler : MonoBehaviour {
+
+    void Update () {
+        if (Input.GetKey(KeyCode.Escape)) {
+            if (SceneManager.GetActiveScene().buildIndex == 0) {
+                Application.Quit();
+            } else {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
